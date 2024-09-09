df460441d0f811d4e889e9211a10892cd623b664
Dimitar I <dimitar.iliev123@gmail.com>
2024-09-09 19:13:20 +0200

    Implementation of all functionalities in Doctors Dashboard

---
+++ Hospital Management System ASP.NET/Migrations/202409091027020_Migration11.cs
@@ -0,0 +1,16 @@
+﻿namespace Hospital_Management_System_ASP.NET.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Migration11 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ Hospital Management System ASP.NET/Migrations/202409091132017_Migration12.cs
@@ -0,0 +1,20 @@
+﻿namespace Hospital_Management_System_ASP.NET.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Migration12 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Patients", "FullName", c => c.String());
+            AlterColumn("dbo.Appointments", "Status", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Appointments", "Status", c => c.String());
+            DropColumn("dbo.Patients", "FullName");
+        }
+    }
+}
