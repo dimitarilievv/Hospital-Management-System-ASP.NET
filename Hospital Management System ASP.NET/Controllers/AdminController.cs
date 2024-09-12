0ec32b8a61cc9b0df1344f3290a46dd9083c2ec9
Dimitar I <dimitar.iliev123@gmail.com>
2024-09-12 22:22:00 +0200

    Add functionalities in Admin Dashboard

--- Hospital Management System ASP.NET/Controllers/AdminController.cs
+++ Hospital Management System ASP.NET/Controllers/AdminController.cs
@@ -9,7 +9,7 @@ using Hospital_Management_System_ASP.NET.ViewModels;
 using Microsoft.AspNet.Identity.Owin;
 using System.Data.Entity;
 using System.Net.Mail;
-
+using System.Net;
 
 namespace Hospital_Management_System_ASP.NET.Controllers
 {
@@ -246,6 +246,301 @@ namespace Hospital_Management_System_ASP.NET.Controllers
             return RedirectToAction("ListOfDoctors");
         }
 
+        //List of Patients
+        [Authorize(Roles = "Admin")]
+        public ActionResult ListOfPatients()
+        {
+            var patientRoleId = db.Roles.SingleOrDefault(r => r.Name == "Patient").Id;
+            var patients = db.Users
+                             .Where(u => u.Roles.Any(r => r.RoleId == patientRoleId))
+                             .Join(db.Patients, u => u.Id, p => p.ApplicationUserId, (u, p) => p)
+                             .ToList();
+            return View(patients);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult EditPatient(int id)
+        {
+            var patient = db.Patients.Single(c => c.PatientId == id);
+            return View(patient);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditPatient(int id, Patient model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var patient = db.Patients.Single(c => c.PatientId == id);
+            patient.FirstName = model.FirstName;
+            patient.LastName = model.LastName;
+            patient.FullName = model.FirstName + " " + model.LastName;
+            patient.Address = model.Address;
+            patient.BloodGroup = model.BloodGroup;
+            patient.DateOfBirth = model.DateOfBirth;
+            patient.EmailAddress = model.EmailAddress;
+            patient.Gender = model.Gender;
+            patient.PhoneNo = model.PhoneNo;
+            db.SaveChanges();
+            return RedirectToAction("ListOfPatients");
+        }
+
+        //Delete Patient
+        [Authorize(Roles = "Admin")]
+        public ActionResult DeletePatient()
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("DeletePatient")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeletePatient(string id)
+        {
+            var patient = db.Patients.Single(c => c.ApplicationUserId == id);
+            var user = db.Users.Single(c => c.Id == id);
+            db.Users.Remove(user);
+            db.Patients.Remove(patient);
+            db.SaveChanges();
+            return RedirectToAction("ListOfPatients");
+        }
+
+        //Add Schedule
+        [Authorize(Roles = "Admin")]
+        public ActionResult AddSchedule()
+        {
+            var collection = new ScheduleDoctorViewModel
+            {
+                Schedule = new Schedule(),
+                Doctors = db.Doctors.ToList()
+            };
+            return View(collection);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddSchedule(ScheduleDoctorViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var collection = new ScheduleDoctorViewModel
+                {
+                    Schedule = model.Schedule,
+                    Doctors = db.Doctors.ToList()
+                };
+                return View(collection);
+            }
+
+            db.Schedules.Add(model.Schedule);
+            db.SaveChanges();
+            return RedirectToAction("ListOfSchedules");
+        }
+
+        //List Of Schedules
+        [Authorize(Roles = "Admin")]
+        public ActionResult ListOfSchedules()
+        {
+            var schedule = db.Schedules.Include(c => c.Doctor).ToList();
+            return View(schedule);
+        }
+
+        //Edit Schedule
+        [Authorize(Roles = "Admin")]
+        public ActionResult EditSchedule(int id)
+        {
+            var collection = new ScheduleDoctorViewModel
+            {
+                Schedule = db.Schedules.Single(c => c.Id == id),
+                Doctors = db.Doctors.ToList()
+            };
+            return View(collection);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditSchedule(int id, ScheduleDoctorViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var schedule = db.Schedules.Single(c => c.Id == id);
+            schedule.DoctorId = model.Schedule.DoctorId;
+            schedule.AvailableEndDay = model.Schedule.AvailableEndDay;
+            schedule.AvailableEndTime = model.Schedule.AvailableEndTime;
+            schedule.AvailableStartDay = model.Schedule.AvailableStartDay;
+            schedule.AvailableStartTime = model.Schedule.AvailableStartTime;
+            schedule.Status = model.Schedule.Status;
+            schedule.TimePerPatient = model.Schedule.TimePerPatient;
+            db.SaveChanges();
+            return RedirectToAction("ListOfSchedules");
+        }
+
+        //Delete Schedule
+        [Authorize(Roles = "Admin")]
+        public ActionResult DeleteSchedule(int? id)
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("DeleteSchedule")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteSchedule(int id)
+        {
+            var schedule = db.Schedules.Single(c => c.Id == id);
+            db.Schedules.Remove(schedule);
+            return RedirectToAction("ListOfSchedules");
+        }
+
+        //List of Active Appointment
+        [Authorize(Roles = "Admin")]
+        public ActionResult ListOfAppointments()
+        {
+            var date = DateTime.Now.Date;
+            var appointment = db.Appointments.Include(c => c.Doctor).Include(c => c.Patient)
+                .Where(c => c.Status == true).Where(c => c.AppointmentTime >= date).ToList();
+            return View(appointment);
+        }
+
+        //List of pending Appointments
+        [Authorize(Roles = "Admin")]
+        public ActionResult PendingAppointments()
+        {
+            var date = DateTime.Now.Date;
+            var appointment = db.Appointments.Include(c => c.Doctor).Include(c => c.Patient)
+                .Where(c => c.Status == false).Where(c => c.AppointmentTime >= date).ToList();
+            return View(appointment);
+        }
+        //Add Appointment
+        [Authorize(Roles = "Admin")]
+        public ActionResult AddAppointment()
+        {
+            var collection = new AppointmentViewModel
+            {
+                Appointment = new Appointment(),
+                Patients = db.Patients.ToList(),
+                Doctors = db.Doctors.ToList()
+            };
+            return View(collection);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddAppointment(AppointmentViewModel model)
+        {
+            var collection = new AppointmentViewModel
+            {
+                Appointment = model.Appointment,
+                Patients = db.Patients.ToList(),
+                Doctors = db.Doctors.ToList()
+            };
+            if (model.Appointment.AppointmentTime >= DateTime.Now.Date)
+            {
+                var appointment = new Appointment();
+                appointment.PatientId = model.Appointment.PatientId;
+                appointment.DoctorId = model.Appointment.DoctorId;
+                appointment.AppointmentTime = model.Appointment.AppointmentTime;
+                appointment.Problem = model.Appointment.Problem;
+                appointment.Status = model.Appointment.Status;
+                db.Appointments.Add(appointment);
+                db.SaveChanges();
+
+                if (model.Appointment.Status == true)
+                {
+                    return RedirectToAction("ListOfAppointments");
+                }
+                else
+                {
+                    return RedirectToAction("PendingAppointments");
+                }
+            }
+
+            ViewBag.Messege = "Please Enter the Date greater than today or equal!!";
+            return View(collection);
+
+        }
+
+        //Edit Appointment
+        [Authorize(Roles = "Admin")]
+        public ActionResult EditAppointment(int id)
+        {
+            var collection = new AppointmentViewModel
+            {
+                Appointment = db.Appointments.Single(c => c.AppointmentId == id),
+                Patients = db.Patients.ToList(),
+                Doctors = db.Doctors.ToList()
+            };
+            return View(collection);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditAppointment(int id, AppointmentViewModel model)
+        {
+            var collection = new AppointmentViewModel
+            {
+                Appointment = model.Appointment,
+                Patients = db.Patients.ToList(),
+                Doctors = db.Doctors.ToList()
+            };
+            if (model.Appointment.AppointmentTime >= DateTime.Now.Date)
+            {
+                var appointment = db.Appointments.Single(c => c.AppointmentId == id);
+                appointment.PatientId = model.Appointment.PatientId;
+                appointment.DoctorId = model.Appointment.DoctorId;
+                appointment.AppointmentTime = model.Appointment.AppointmentTime;
+                appointment.Problem = model.Appointment.Problem;
+                appointment.Status = model.Appointment.Status;
+                db.SaveChanges();
+                if (model.Appointment.Status == true)
+                {
+                    return RedirectToAction("ListOfAppointments");
+                }
+                else
+                {
+                    return RedirectToAction("PendingAppointments");
+                }
+            }
+            ViewBag.Messege = "Please Enter the Date greater than today or equal!!";
+
+            return View(collection);
+        }
+
+        //Detail of Appointment
+        [Authorize(Roles = "Admin")]
+        public ActionResult DetailOfAppointment(int id)
+        {
+            var appointment = db.Appointments.Include(c => c.Doctor).Include(c => c.Patient).Single(c => c.AppointmentId == id);
+            return View(appointment);
+        }
+
+        //Delete Appointment
+        [Authorize(Roles = "Admin")]
+        public ActionResult DeleteAppointment(int? id)
+        {
+            var appointment = db.Appointments.Single(c => c.AppointmentId == id);
+            return View(appointment);
+        }
+
+        [HttpPost, ActionName("DeleteAppointment")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteAppointment(int id)
+        {
+            var appointment = db.Appointments.Single(c => c.AppointmentId == id);
+            db.Appointments.Remove(appointment);
+            db.SaveChanges();
+            if (appointment.Status)
+            {
+                return RedirectToAction("ListOfAppointments");
+            }
+            else
+            {
+                return RedirectToAction("PendingAppointments");
+            }
+        }
 
 
     }
---
+++ Hospital Management System ASP.NET/ViewModels/ScheduleDoctorViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Hospital_Management_System_ASP.NET.Models;
+
+namespace Hospital_Management_System_ASP.NET.ViewModels
+{
+    public class ScheduleDoctorViewModel
+    {
+        public Schedule Schedule { get; set; }
+        public IEnumerable<Doctor> Doctors { get; set; }
+    }
+}
